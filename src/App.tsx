544eb798a52e7bba08b7f72da6c57f699b12eb3c
Ryder <rbiscup@outlook.com>
2023-04-10 14:37:36 -0500

    added the rest of the products

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Banner from "./components/banner";
 import Quotes from "./components/quotes";
 import { useState } from "react";
 // import style from "./text";
 import CQuote from "./models/Quote";
 import PleaseWait from "./components/PleaseWait";
-import './App.css';
-import CProduct from './models/Product';
+import "./App.css";
+import CProduct from "./models/Product";
 
 const substyle = {
   fontWeight: "bold",
@@ -20,26 +20,24 @@ const App = () => {
   const [m_dToolingPrice, setm_dToolingPrice] = useState("");
 
   const [m_nABBIRB4600qty, setm_nABBIRB4600qty] = useState(0);
-  const [m_nIRBP1000qty, setm_nIRBP1000qty] = useState("");
-  const [m_nABBBullsEyeqty, setm_nABBBullsEyeqty] = useState("");
-  const [m_nAbicorBinzelqty, setm_nAbicorBinzelqty] = useState("");
-  const [m_nFroniusTPSi400qty, setm_nFroniusTPSi400qty] = useState("");
+  const [m_nIRBP1000qty, setm_nIRBP1000qty] = useState(0);
+  const [m_nABBBullsEyeqty, setm_nABBBullsEyeqty] = useState(0);
+  const [m_nAbicorBinzelqty, setm_nAbicorBinzelqty] = useState(0);
+  const [m_nFroniusTPSi400qty, setm_nFroniusTPSi400qty] = useState(0);
   const [m_nKeyencesafetyareascannersqty, setm_nKeyencesafetyareascannersqty] =
-    useState("");
+    useState(0);
   const [m_nMinitecfencingsystemqty, setm_nMinitecfencingsystemqty] =
-    useState("");
+    useState(0);
   const [m_nPushbuttonoperatorpanelsqty, setm_nPushbuttonoperatorpanelsqty] =
-    useState("");
-  const [m_ncablecoversqty, setm_ncablecoversqty] = useState("");
-  const [m_nDigitalgasflowmeterqty, setm_nDigitalgasflowmeterqty] =
-    useState("");
-  const [m_nFroniusConsumablesqty, setm_nFroniusConsumablesqty] = useState("");
-  const [m_nWirewizardconduitqty, setm_nWirewizardconduitqty] = useState("");
-  const [m_nPendantarmorqty, setm_nPendantarmorqty] = useState("");
-  const [m_nRemoteaccessmoduleqty, setm_nRemoteaccessmoduleqty] = useState("");
-  const [m_nElectrodetouchsenseqty, setm_nElectrodetouchsenseqty] =
-    useState("");
-  const [m_nSafetyPLCqty, setm_nSafetyPLCqty] = useState("");
+    useState(0);
+  const [m_ncablecoversqty, setm_ncablecoversqty] = useState(0);
+  const [m_nDigitalgasflowmeterqty, setm_nDigitalgasflowmeterqty] = useState(0);
+  const [m_nFroniusConsumablesqty, setm_nFroniusConsumablesqty] = useState(0);
+  const [m_nWirewizardconduitqty, setm_nWirewizardconduitqty] = useState(0);
+  const [m_nPendantarmorqty, setm_nPendantarmorqty] = useState(0);
+  const [m_nRemoteaccessmoduleqty, setm_nRemoteaccessmoduleqty] = useState(0);
+  const [m_nElectrodetouchsenseqty, setm_nElectrodetouchsenseqty] = useState(0);
+  const [m_nSafetyPLCqty, setm_nSafetyPLCqty] = useState(0);
 
   const [m_bShowPleaseWait, setm_bShowPleaseWait] = useState(false);
 
@@ -52,6 +50,97 @@ const App = () => {
       let cp: CProduct = new CProduct();
       cp.name = "ABBIRB4600";
       cp.quantity = m_nABBIRB4600qty;
+      v.products.push(cp);
+    }
+    if (m_nIRBP1000qty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "IRBP1000";
+      cp.quantity = m_nIRBP1000qty;
+      v.products.push(cp);
+    }
+    if (m_nABBBullsEyeqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "ABBBullsEye";
+      cp.quantity = m_nABBBullsEyeqty;
+      v.products.push(cp);
+    }
+    if (m_nAbicorBinzelqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "AbicorBinzel";
+      cp.quantity = m_nAbicorBinzelqty;
+      v.products.push(cp);
+    }
+    if (m_nFroniusTPSi400qty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "FroniusTPSi400";
+      cp.quantity = m_nFroniusTPSi400qty;
+      v.products.push(cp);
+    }
+    if (m_nKeyencesafetyareascannersqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "Keyencesafetyareascanners";
+      cp.quantity = m_nKeyencesafetyareascannersqty;
+      v.products.push(cp);
+    }
+    if (m_nMinitecfencingsystemqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "Minitecfencingsystem";
+      cp.quantity = m_nMinitecfencingsystemqty;
+      v.products.push(cp);
+    }
+    if (m_nPushbuttonoperatorpanelsqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "Pushbuttonoperatorpanels";
+      cp.quantity = m_nPushbuttonoperatorpanelsqty;
+      v.products.push(cp);
+    }
+    if (m_ncablecoversqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "cablecovers";
+      cp.quantity = m_ncablecoversqty;
+      v.products.push(cp);
+    }
+    if (m_nDigitalgasflowmeterqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "Digitalgasflowmeter";
+      cp.quantity = m_nDigitalgasflowmeterqty;
+      v.products.push(cp);
+    }
+    if (m_nFroniusConsumablesqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "FroniusConsumables";
+      cp.quantity = m_nFroniusConsumablesqty;
+      v.products.push(cp);
+    }
+    if (m_nWirewizardconduitqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "Wirewizardconduit";
+      cp.quantity = m_nWirewizardconduitqty;
+      v.products.push(cp);
+    }
+    if (m_nPendantarmorqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "Pendantarmor";
+      cp.quantity = m_nPendantarmorqty;
+      v.products.push(cp);
+    }
+    if (m_nRemoteaccessmoduleqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "Remoteaccessmoduleq";
+      cp.quantity = m_nRemoteaccessmoduleqty;
+      v.products.push(cp);
+    }
+
+    if (m_nElectrodetouchsenseqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "Electrodetouchsense";
+      cp.quantity = m_nElectrodetouchsenseqty;
+      v.products.push(cp);
+    }
+    if (m_nSafetyPLCqty > 0) {
+      let cp: CProduct = new CProduct();
+      cp.name = "SafetyPLC";
+      cp.quantity = m_nSafetyPLCqty;
       v.products.push(cp);
     }
 
@@ -75,10 +164,17 @@ const App = () => {
   }
 
   if (m_bShowPleaseWait)
-    return <div className="container"><div className="row"><div className="col"><PleaseWait Show={m_bShowPleaseWait}></PleaseWait></div></div></div>
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <PleaseWait Show={m_bShowPleaseWait}></PleaseWait>
+          </div>
+        </div>
+      </div>
+    );
   else
     return (
-
       <div className="container">
         <div className="row">
           <div className="col">
@@ -87,7 +183,7 @@ const App = () => {
         </div>
         <div className="row">
           <div className="col-2">
-            <b>Company Name</b>
+            <b>Company Name:</b>
           </div>
           <div className="col">
             <input
@@ -97,10 +193,12 @@ const App = () => {
             ></input>
           </div>
         </div>
-        <div className="row"><div className="col-2">&nbsp;</div></div>
+        <div className="row">
+          <div className="col-2">&nbsp;</div>
+        </div>
         <div className="row">
           <div className="col-2">
-            <b>Tooling Description</b>
+            <b>Tooling Description:</b>
           </div>
           <div className="col">
             <textarea
@@ -110,10 +208,12 @@ const App = () => {
             ></textarea>
           </div>
         </div>
-        <div className="row"><div className="col-2">&nbsp;</div></div>
+        <div className="row">
+          <div className="col-2">&nbsp;</div>
+        </div>
         <div className="row">
           <div className="col-2">
-            <b>Tooling Price</b>
+            <b>Tooling Price:</b>
           </div>
           <div className="col">
             <input
@@ -124,16 +224,18 @@ const App = () => {
             ></input>
           </div>
         </div>
-        <div className="row"><div className="col-2">&nbsp;</div></div>
         <div className="row">
-          <div className="col-2">
-            <b>Options</b>
+          <div className="col-2">&nbsp;</div>
+        </div>
+        <div className="row">
+          <div className="col-2 mt-2">
+            <b>Options:</b>
           </div>
           <div className="col">
             <table className="table">
               <thead>
                 <tr>
-                  <th scope="col">qty</th>
+                  <th scope="col">Quantity</th>
                   <th scope="col">Product</th>
                 </tr>
               </thead>
@@ -144,7 +246,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nABBIRB4600qty}
-                      onChange={(e) => setm_nABBIRB4600qty(Number(e.target.value))}
+                      onChange={(e) =>
+                        setm_nABBIRB4600qty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>ABBIRB4600-20kg2.5-meterroboticweldingarm</td>
@@ -155,7 +259,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nIRBP1000qty}
-                      onChange={(e) => setm_nIRBP1000qty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nIRBP1000qty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>IRB-P1000Lheadtailstockpositioners</td>
@@ -166,7 +272,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nABBBullsEyeqty}
-                      onChange={(e) => setm_nABBBullsEyeqty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nABBBullsEyeqty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>ABB BullsEye tool alignment station</td>
@@ -177,7 +285,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nAbicorBinzelqty}
-                      onChange={(e) => setm_nAbicorBinzelqty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nAbicorBinzelqty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>Abicor Binzel TCS-PP torch cleaning station</td>
@@ -188,7 +298,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nFroniusTPSi400qty}
-                      onChange={(e) => setm_nFroniusTPSi400qty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nFroniusTPSi400qty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>
@@ -202,7 +314,9 @@ const App = () => {
                       className=" "
                       value={m_nKeyencesafetyareascannersqty}
                       onChange={(e) =>
-                        setm_nKeyencesafetyareascannersqty(e.target.value)
+                        setm_nKeyencesafetyareascannersqty(
+                          Number(e.target.value)
+                        )
                       }
                     />
                   </th>
@@ -215,13 +329,13 @@ const App = () => {
                       className=" "
                       value={m_nMinitecfencingsystemqty}
                       onChange={(e) =>
-                        setm_nMinitecfencingsystemqty(e.target.value)
+                        setm_nMinitecfencingsystemqty(Number(e.target.value))
                       }
                     />
                   </th>
                   <td>
-                    Minitec fencing system with interlocked door and pop up flash
-                    screens
+                    Minitec fencing system with interlocked door and pop up
+                    flash screens
                   </td>
                 </tr>
                 <tr>
@@ -231,7 +345,9 @@ const App = () => {
                       className=" "
                       value={m_nPushbuttonoperatorpanelsqty}
                       onChange={(e) =>
-                        setm_nPushbuttonoperatorpanelsqty(e.target.value)
+                        setm_nPushbuttonoperatorpanelsqty(
+                          Number(e.target.value)
+                        )
                       }
                     />
                   </th>
@@ -243,7 +359,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_ncablecoversqty}
-                      onChange={(e) => setm_ncablecoversqty(e.target.value)}
+                      onChange={(e) =>
+                        setm_ncablecoversqty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>Set of cable covers, robot riser, and floor plates</td>
@@ -254,7 +372,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nDigitalgasflowmeterqty}
-                      onChange={(e) => setm_nDigitalgasflowmeterqty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nDigitalgasflowmeterqty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>Digital gas flow meter </td>
@@ -265,12 +385,14 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nFroniusConsumablesqty}
-                      onChange={(e) => setm_nFroniusConsumablesqty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nFroniusConsumablesqty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>
-                    Fronius Consumables- tips, nozzles, spatter guards, diffusers,
-                    liner, and coolant
+                    Fronius Consumables- tips, nozzles, spatter guards,
+                    diffusers, liner, and coolant
                   </td>
                 </tr>
                 <tr>
@@ -279,7 +401,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nWirewizardconduitqty}
-                      onChange={(e) => setm_nWirewizardconduitqty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nWirewizardconduitqty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>Wire wizard conduit, fittings, and barrel cover</td>
@@ -290,7 +414,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nPendantarmorqty}
-                      onChange={(e) => setm_nPendantarmorqty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nPendantarmorqty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>Pendant armor cover for teach pendant </td>
@@ -301,7 +427,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nRemoteaccessmoduleqty}
-                      onChange={(e) => setm_nRemoteaccessmoduleqty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nRemoteaccessmoduleqty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>Remote access module </td>
@@ -312,7 +440,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nElectrodetouchsenseqty}
-                      onChange={(e) => setm_nElectrodetouchsenseqty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nElectrodetouchsenseqty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>Electrode touch sense </td>
@@ -323,7 +453,9 @@ const App = () => {
                       type="number"
                       className=" "
                       value={m_nSafetyPLCqty}
-                      onChange={(e) => setm_nSafetyPLCqty(e.target.value)}
+                      onChange={(e) =>
+                        setm_nSafetyPLCqty(Number(e.target.value))
+                      }
                     />
                   </th>
                   <td>Safety PLC, electronic position switch</td>
@@ -332,29 +464,28 @@ const App = () => {
             </table>
           </div>
         </div>
-        <div className="row"><div className="col-2">&nbsp;</div></div>
         <div className="row">
-          <div className="col-2">
-          </div>
+          <div className="col-2">&nbsp;</div>
+        </div>
+        <div className="row">
+          <div className="col-2"></div>
           <div className="col">
             <button
               onClick={() => Process()}
-              className="btn btn-success buttonwidth"
+              className="btn btn-success buttonwidth "
             >
               Generate
             </button>
-
           </div>
         </div>
-        <div className="row"><div className="col-2">&nbsp;</div></div>
+        <div className="row">
+          <div className="col-2">&nbsp;</div>
+        </div>
         <div className="row">
           <div className="col">
             <Quotes></Quotes>
           </div>
-
         </div>
-
-
       </div>
     );
 };
