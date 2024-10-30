9790ce54ff3695ac8448af3fe154073862ee16ab
Ryder Biscup <rbiscup@outlook.com>
2024-10-30 11:25:32 -0500

    more S

--- src/Pages/InfoPage.tsx
+++ src/Pages/InfoPage.tsx
@@ -36,7 +36,7 @@ const InfoPage = (props: any) => {
 
   const autoFill = () => {
     axios
-      .get(`http://4.236.227.113:5000/GetCompanyInfo?quotenumber=${quoteNumber}`)
+      .get(`https://4.236.227.113:5000/GetCompanyInfo?quotenumber=${quoteNumber}`)
       .then((response) => {
         props.FirstName(response.data.person_name);
         props.Company(response.data.org_name);
